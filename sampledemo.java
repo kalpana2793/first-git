97cafbf5fd29ba9873e21c9abbc4d0a2adeafc5d
kalpana2793 <94058206+kalpana2793@users.noreply.github.com>
2021-11-10 06:03:53 -0800

    Add files via upload

---
+++ sampledemo.java
@@ -0,0 +1,20 @@
+package com.selenium.demo;
+
+public class sampledemo {
+
+		public void method1()
+		{
+			System.out.println("Method1");
+
+		}
+		
+		public void method2()
+		{
+			System.out.println("Method2");
+
+		}
+
+
+	}
+
+
