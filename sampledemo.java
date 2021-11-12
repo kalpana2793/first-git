3b58a320cfa4b6d85f2df0a59db0b456433dbd7f
kalpana2793 <kalpana2793@gmail.com>
2021-11-11 19:54:43 -0800

    test cases added

---
+++ sampledemo.java
@@ -0,0 +1,26 @@
+package com.selenium.demo;
+
+public class sampledemo {
+
+		public void method1()
+		{
+			System.out.println("Method1");
+
+		}
+		
+		public void method2()
+		{
+			System.out.println("Method2");
+
+		}
+
+		public void method3()
+		{
+			System.out.println("Method3");
+
+		}
+
+
+	}
+
+
